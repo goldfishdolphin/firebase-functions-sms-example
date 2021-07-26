13c52efbb569583ef34e398f71f04b643c507080
Amanda Cavallaro <amanda.cavallaro@vonage.com>
2021-07-26 11:15:36 +0100

    Adds changes to index.js and .env.example

--- functions/index.js
+++ functions/index.js
@@ -8,37 +8,46 @@ admin.initializeApp();
 
 const vonage = new Vonage({
   apiKey: process.env.VONAGE_API_KEY,
-  apiSecret: process.env.VONAGE_API_SECRET
+  apiSecret: process.env.VONAGE_API_SECRET,
 });
-
-const from = process.env.VONAGE_FROM_NUMBER;
-const to = process.env.VONAGE_TO_NUMBER;
-const text = 'A text message sent using the Vonage SMS API';
 
 // This function will serve as the webhook for incoming SMS messages,
 // and will log the message into the Firebase Realtime Database
 exports.inboundSMS = functions.https.onRequest(async (req, res) => {
-  const params = Object.assign(req.query, req.body);
+  let params;
+  if (Object.keys(req.query).length === 0) {
+    params = req.body;
+  } else {
+    params = req.query;
+  }
   await admin.database().ref('/msgq').push(params);
   res.sendStatus(200);
 });
 
 // This function listens for updates to the Firebase Realtime Database
 // and sends a message back to the original sender
-exports.sendSMS = functions.database.ref('/msgq/{pushId}')
-  .onCreate(async (snapshot) => {
+exports.sendSMS = functions.database
+  .ref('/msgq/{pushId}')
+  .onCreate(async (message) => {
+    const { msisdn, text, to } = message.val();
     const result = await new Promise((resolve, reject) => {
-      vonage.message.sendSms(from, to, text, (err, responseData) => {
+      vonage.message.sendSms(msisdn, to, `You sent the following text: ${text}`, (err, responseData) => {
         if (err) {
           return reject(new Error(err));
         } else {
-          if (responseData.messages[0]['status'] === "0") {
-            return resolve(`Message sent successfully: ${responseData.messages[0]['message-id']}`);
+          if (responseData.messages[0].status === '0') {
+            return resolve(
+              `Message sent successfully: ${responseData.messages[0]['message-id']}`
+            );
           } else {
-            return reject(new Error(`Message failed with error: ${responseData.messages[0]['error-text']}`));
+            return reject(
+              new Error(
+                `Message failed with error: ${responseData.messages[0]['error-text']}`
+              )
+            );
           }
         }
       });
     });
-    return snapshot.ref.parent.child('result').set(result);
+    return message.ref.parent.child('result').set(result);
   });
